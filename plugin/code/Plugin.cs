fe892e076eb1b71efd0d1a5c693f09528326810c
The G-Meister <thegiemeister@gmail.com>
2025-08-12 13:41:16 +0100

    initial commit

---
+++ plugin/code/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace dev.gmeister.unsighted.modloader.code;
+
+[BepInPlugin(GUID, NAME, VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+
+    public const string GUID = "dev.gmeister.unsighted.modloader";
+    public const string NAME = "Unsighted Mod API";
+    public const string VERSION = "0.0.1";
+
+    public static Plugin instance;
+
+    public Plugin()
+    {
+        if (Plugin.instance != null) throw new ApplicationException("Cannot create another instance of Plugin as one already exists");
+
+        new Harmony(GUID).PatchAll();
+    }
+
+}
